3b07f8072437bb50429e946e3edcf97fcac7c0da
Daniel Truemper <truemper@retresco.de>
2014-05-28 10:57:14 +0200

    Bad start script I guess

--- bbhack/streamer.py
+++ bbhack/streamer.py
@@ -56,12 +56,11 @@ def main():
     """Start the Twitter streamer"""
 
     import argparse
-    p = argparse.ArgumentParser(
-        help='For Twitter credentials see https://apps.twitter.com/')
-    p.add_argument('consumer_key')
-    p.add_argument('consumer_secret')
-    p.add_argument('access_token')
-    p.add_argument('access_token_secret')
+    p = argparse.ArgumentParser()
+    p.add_argument('--consumer_key')
+    p.add_argument('--consumer_secret')
+    p.add_argument('--access_token')
+    p.add_argument('--access_token_secret')
     p.add_argument('--zmq_pub_string', default='tcp://*:5556')
     p.add_argument('--track', default='twitter')
 
